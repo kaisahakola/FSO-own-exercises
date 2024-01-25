2389ba9a7284b15ffb8796241e510d217639ac42
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-25 10:55:08 +0200

    Uudelleenrenderöinti

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,38 +1,7 @@
-const Hello = ({name, age}) => {
-  // Destrukturointi, voidaan laittaa myös suoraan komponenttifunktion parametreiksi
-  // const { name, age } = props
-
-  // Nuolifunktion hyödyntäminen
-  const bornYear = () => new Date().getFullYear() - age
-
-  // Toimii myös näin:
-  /*
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - age
-  }
-  */
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born {bornYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
