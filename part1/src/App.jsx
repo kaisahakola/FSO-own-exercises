b2ab6d3e2440a0617b2fb58d852a2f9d512b67f8
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-25 11:29:06 +0200

    Nuolifuntkion lyhentäminen

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,11 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({handleClick, text}) => {
-
-  return (
-    <>
-      <button onClick={handleClick}>
-        {text}
-      </button>
-    </>
-  )
+  <button onClick={handleClick}>
+    {text}
+  </button>
 }
 
 const App = () => {
