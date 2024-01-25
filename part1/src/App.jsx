b8b1610d0eeb60f984bd32339530977703f8bceb
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-25 12:31:45 +0200

    Tapahtumankäsittelijän vienti alikomponenttiin

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,29 +1,27 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+const Display = value => <div>{value}</div>
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <p>{allClicks.join(' ')}</p>
-      </div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text='thousand' />
+      <Button handleClick={() => setToValue(0)} text='zero' />
+      <Button handleClick={() => setToValue(value + 1)} text='increment' />
     </div>
   )
 }
