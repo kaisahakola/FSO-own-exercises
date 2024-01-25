a1e080b41a73f1eedc8295fb4c1e9be7371569ed
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-01-25 11:39:53 +0200

    Monimutkaisempi tila ja taulukon käsittelyä

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,40 +1,30 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({handleClick, text}) => {
-  <button onClick={handleClick}>
-    {text}
-  </button>
-}
-
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value ', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before ', counter)
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
-  const decreaseByOne = () => {
-    console.log('decreasing, value before ', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before ', counter)
-    setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text='plus' />
-      <Button handleClick={setToZero} text='zero' />
-      <Button handleClick={decreaseByOne} text='minus' />
-    </>
+    <div>
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
+      </div>
+    </div>
   )
 }
 
