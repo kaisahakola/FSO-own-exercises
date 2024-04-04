81bb82458b0cb85db23d7df70cd7f4ee39cf0b0f
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-04-04 11:55:14 +0300

    6D

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux' 
 import store from './store'
 import App from './App'
@@ -6,6 +6,5 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
