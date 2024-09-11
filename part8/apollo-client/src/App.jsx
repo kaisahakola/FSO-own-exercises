c1d80ea3743b526f845c5bf20d74ddfa2ee85374
Kaisa Hakola <kaisahakola@Kaisa-MBP.zyxel.com>
2024-09-11 14:55:09 +0300

    8 a & b

---
+++ part8/apollo-client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_PERSONS } from './queries'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm'
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm />
+    </div>
+  )
+}
+
+export default App
